da406383e66246a2f7eefeab38b6bd51f50e7ce0
Sergey Fadeev <fadeev@gmail.com>
2014-01-05 00:08:56 +0400

    renamed ISourceProcessor to ISourceTransformer

--- Nocco/ISourceProcessor.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nocco
-{
-	// Interface to process input sources to html documentation.
-	public interface ISourceProcessor
-	{
-		string Process(SourceInfo source);
-	}
-}
---
+++ Nocco/ISourceTransformer.cs
@@ -0,0 +1,8 @@
+﻿namespace Nocco
+{
+	// Simple interface to transform input sources to html documentation.
+	public interface ISourceTransformer
+	{
+		string Transform(SourceInfo source);
+	}
+}
